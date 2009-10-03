15922cf7f3677ffc9c1e394c815baff70f7a95de
Guillaume Watteeux <guillaume.watteeux@gmail.com>
2009-10-03 19:40:54 +0000

    First commit for Centreon2 debian package

---
+++ centreon2/misc/centreon2.conf.php
@@ -0,0 +1,17 @@
+<?php
+
+	/* Require database config */
+	if (file_exists("/etc/centreon2/centreon2.db.conf.php")) {
+		require_once '/etc/centreon2/centreon2.db.conf.php';
+	}
+
+	if (file_exists("/etc/centreon2/centstorage.db.conf.php")) {
+		require_once '/etc/centreon2/centstorage.db.conf.php';
+	}
+
+	/* Path to classes */
+	$classdir='./class';
+	
+	/* Centreon2 Path */
+	$centreon_path='/usr/share/centreon2';
+?>
